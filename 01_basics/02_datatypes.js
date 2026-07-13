74b571c0bff38a0c6aefe7c7085aa829c0127a92
Shivaling A Kurennavar <shivaling.a.kurennavar@gmail.com>
2026-07-13 17:00:27 +0000

    Day 1

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // Treat all js code as newer version
+
+// alert("Hello Js"); // We are using node.js not a browser
+
+console.log(3
+    +3); // Code readability should be high
+
+console.log("Welcome bro");
+    
+
+let name = "Shivaling";
+let age = 22;
+let isLoggedIn = true;
+let state;
+
+// Number => 2 to power 53
+// BigInt
+// String => " "
+// Boolean => true/false
+// Null => stand alone value (It is an object)
+// Undefined => Not assigned value yet
+// Symbol => Unique
+
+// Object
+
+console.log(typeof Number);
+
+console.log(typeof name);
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
