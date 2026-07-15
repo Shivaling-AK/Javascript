bc4061ef440beeec4db7414615727fcce7dd695d
Shivaling A Kurennavar <shivaling.a.kurennavar@gmail.com>
2026-07-15 17:43:03 +0000

    Day 2 -> The conversion of the datatypes.

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,44 @@
+// let score =  17;
+
+// console.log(typeof score);
+
+// let score1 = "10";
+// console.log(typeof score1);
+
+// let score2 = "123abc";
+// console.log(typeof score2);
+
+// let  valueInNumber = Number(score2);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score4 = null;
+// let valueInNumber1 = Number(score4);
+// console.log(typeof valueInNumber1);
+// console.log(valueInNumber1);
+
+// let score5 = undefined;
+// let valueInNumber2 = Number(score5);
+// console.log(typeof valueInNumber2);
+// console.log(valueInNumber2);
+
+// "33" -> 33
+// "33abc" -> NaN
+// true -> 1 & false -> 0
+
+
+let isLoggedIn = "Shiva";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 -> true & 0 -> false
+// "" -> false
+// "Shiva" -> true
+
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
